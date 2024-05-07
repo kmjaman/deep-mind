064505ec99ead23492724e077bb9bd75ae46c6b7
Kamrujjaman Md <jaman.edux@gmail.com>
2024-05-08 01:22:09 +0200

    Use best hyperparameters to train model

---
+++ training/train_with_best_hyperparameters.py
@@ -0,0 +1,45 @@
+import sys
+sys.path.append('/path/to/project/')
+
+from hyperparameter_tuning.tune_hyperparameters import run_hyperparameter_tuning
+from model.base_model import BaseTransformer
+from model.config import TransformerConfig
+from training.train import train_model
+from data.text_dataset import TextDataset
+from data.preprocess_custom_data import load_custom_data
+from utils.vocabulary import Vocabulary
+from utils.tokenizer import Tokenizer
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+# Get best parameters
+best_params = run_hyperparameter_tuning()
+
+# Prepare dataset
+file_path = "custom_data.txt"
+custom_texts = load_custom_data(file_path)
+
+vocab = Vocabulary()
+for text in custom_texts:
+    for token in text.split():
+        vocab.add_token(token)
+
+tokenizer = Tokenizer(vocab)
+dataset = TextDataset(custom_texts, tokenizer, max_length=20)
+dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
+
+# Configure and train model
+config = TransformerConfig(
+    vocab_size=len(vocab),
+    num_layers=best_params["num_layers"],
+    num_heads=best_params["num_heads"],
+    ff_hidden_size=best_params["hidden_dim"],
+    dropout=best_params["dropout"],
+)
+model = BaseTransformer(config)
+optimizer = torch.optim.Adam(model.parameters(), lr=best_params["learning_rate"])
+loss_fn = nn.CrossEntropyLoss(ignore_index=vocab.get_index("[PAD]"))
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+train_model(model, dataloader, optimizer, loss_fn, device, epochs=10)
